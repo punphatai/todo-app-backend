d2f242a09b8f9b26c15b6008b57a91a2d38f6f30
punphatai <punphatai@kkumail.com>
2022-12-06 17:33:39 +0700

    Add UserController.java

---
+++ src/main/java/com/dev/todoapp/user/UserController.java
@@ -0,0 +1,22 @@
+package com.dev.todoapp.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(path = "api/v1/user")
+public class UserController {
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+    @GetMapping("get-all-user")
+    public List<User> getUser() {
+        return this.userService.getUser();
+    }
+
+}

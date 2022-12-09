667f75bf46dd0c11cdde0aedae5d22aa406c17e1
punphatai <punphatai@kkumail.com>
2022-12-10 04:56:32 +0700

    add query for method findTaskByUserId

--- src/main/java/com/dev/todoapp/task/TaskRepository.java
+++ src/main/java/com/dev/todoapp/task/TaskRepository.java
@@ -1,7 +1,14 @@
 package com.dev.todoapp.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface TaskRepository
         extends JpaRepository<Task, Integer> {
+
+    @Query("SELECT t FROM Task t where t.user.id = :user_id")
+    List<Task> findTaskByUserId(@Param("user_id") int user_id);
 }

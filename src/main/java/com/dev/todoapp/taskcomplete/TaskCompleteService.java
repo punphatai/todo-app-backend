18eaa0a1cd95ff876fa0c5121f2e07a2d83b4626
punphatai <punphatai@kkumail.com>
2022-12-11 06:36:52 +0700

    add addTaskCompleteByUserId for TaskCompleteService

--- src/main/java/com/dev/todoapp/taskcomplete/TaskCompleteService.java
+++ src/main/java/com/dev/todoapp/taskcomplete/TaskCompleteService.java
@@ -1,6 +1,7 @@
 package com.dev.todoapp.taskcomplete;
 
 import com.dev.todoapp.task.Task;
+import com.dev.todoapp.user.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,4 +21,13 @@ public class TaskCompleteService {
         return this.taskCompleteRepository.findAll();
     }
 
+    public void addTaskCompleteByUserId(int user_id, int task_id) {
+        User user = new User(user_id);
+        Task task = new Task(task_id);
+        TaskComplete taskComplete = new TaskComplete(
+                user,
+                task
+        );
+        this.taskCompleteRepository.save(taskComplete);
+    }
 }

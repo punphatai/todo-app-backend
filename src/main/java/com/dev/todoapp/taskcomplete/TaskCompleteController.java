721645adc7b8bd607c55f1de78c30640657ffc75
punphatai <punphatai@kkumail.com>
2022-12-11 06:36:10 +0700

    add Post method for addTaskCompleteByUserId

--- src/main/java/com/dev/todoapp/taskcomplete/TaskCompleteController.java
+++ src/main/java/com/dev/todoapp/taskcomplete/TaskCompleteController.java
@@ -1,8 +1,6 @@
 package com.dev.todoapp.taskcomplete;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -19,5 +17,10 @@ public class TaskCompleteController {
     @GetMapping("")
     public List<TaskComplete> getTaskComplete() {
         return this.taskCompleteService.getTaskComplete();
+    }
+
+    @PostMapping("add-task-complete-by-user-id/{user_id}/{task_id}")
+    public void addTaskCompleteByUserId(@PathVariable("user_id") int user_id, @PathVariable("task_id") int task_id) {
+        this.taskCompleteService.addTaskCompleteByUserId(user_id, task_id);
     }
 }
